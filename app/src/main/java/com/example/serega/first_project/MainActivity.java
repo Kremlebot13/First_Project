244d0225a987249e36cbb71614ea6d91ade426c2
Sergey Osyonov <master13rup@gmail.com>
2018-12-17 16:42:37 +0300

    task 3

--- app/src/main/java/com/example/serega/first_project/MainActivity.java
+++ app/src/main/java/com/example/serega/first_project/MainActivity.java
@@ -5,6 +5,29 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static void main(String[] args) {
+        task3();
+    }
+
+    //Problem 3
+    static void task3() {
+        for (int i = 0; i <= 15; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+        System.out.println();
+    }
+
+    static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
