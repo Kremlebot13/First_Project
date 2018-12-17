f0a8f8986533c47c2e33d4117ee489f4656a32fc
Sergey Osyonov <master13rup@gmail.com>
2018-12-17 17:47:39 +0300

    task 5

--- app/src/main/java/com/example/serega/first_project/MainActivity.java
+++ app/src/main/java/com/example/serega/first_project/MainActivity.java
@@ -5,6 +5,33 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static void main(String[] args) {
+        System.out.println(task5("abc123"));
+    }
+
+    //problem 5
+    static String task5(String str) {
+        int index = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) <= '9' && str.charAt(i) >= '0') {
+                index = i;
+                break;
+            }
+        }
+
+        String line = str.substring(0, index);
+        String num = str.substring(index, str.length());
+
+        try {
+            int number = Integer.parseInt(num);
+            number++;
+            line += number;
+        } catch (NumberFormatException e) {
+            line = str.substring(0, str.length());
+        }
+        return line;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
