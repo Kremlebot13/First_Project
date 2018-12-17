70e5b0212460efb6f944126b19273ea6f7bfc475
Sergey Osyonov <master13rup@gmail.com>
2018-12-17 16:51:29 +0300

    task 4

--- app/src/main/java/com/example/serega/first_project/MainActivity.java
+++ app/src/main/java/com/example/serega/first_project/MainActivity.java
@@ -5,6 +5,34 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static void main(String[] args) {
+        task4();
+    }
+
+    //Problem 4
+    static void task4() {
+        int array[] = {3, 17, 7, 9, 5, 1, -1, -6, 199, 435, 36};
+        array = bubble_sort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static int[] bubble_sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
