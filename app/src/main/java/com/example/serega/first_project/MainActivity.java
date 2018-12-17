d9ea975f64c222ab4cf7fe33bc38ea1138bf5770
Sergey Osyonov <master13rup@gmail.com>
2018-12-17 16:14:23 +0300

    task 1

--- app/src/main/java/com/example/serega/first_project/MainActivity.java
+++ app/src/main/java/com/example/serega/first_project/MainActivity.java
@@ -5,6 +5,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static void main(String[] args) {
+        task1();
+    }
+
+    //Problem 1
+    static void task1() {
+        final double a = 3.2567;
+        final double b = 12.02342;
+        double average = averageValue(a, b);
+        System.out.println(average);
+    }
+
+    //arithmetic average of two numbers
+    static double averageValue(double a, double b) {
+        return (a + b) / 2;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
